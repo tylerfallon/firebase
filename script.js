4735a197d3104f679553967317852aaa7a4b00db
Tyler Fallon <Fallon@Tyler-Fallons-MacBook-Pro.local>
2016-06-17 17:38:20 -0400

    added more columns

--- script.js
+++ script.js
@@ -9,47 +9,35 @@ var sessionRef = dataRef.child('trainTime')
 		var destination = $('#destination').val();
 		var time = $('#frequency').val();
 		var frequency = $('#traintime').val();
+		var minaway = $('#minaway').val();
 		dataRef.push({
 			name: name,
 			destination: destination, 
 			time: time,
-			frequency: frequency
+			frequency: frequency,
+			minaway: minaway
 		});
 
 		$('#name').val('');
 		$('#destination').val('');
 		$('#frequency').val('');
 		$('#traintime').val('');
+		$('#minaway').val('');
 		return false;
 	});
 
 
-dataRef.on("child_added", function(snapshot) {
-
-
-	// Create a new Table row. outside of the for loop 	
+dataRef.on("child_added", function(snapshot) { 	
 	var tableRow = $('<tr>');	
-
-	// add all train information to an array for easy use 
-	var trainInfo = [snapshot.val().name, snapshot.val().destination, snapshot.val().time, snapshot.val().frequency];
+	var trainInfo = [snapshot.val().name, snapshot.val().destination, snapshot.val().time, snapshot.val().frequency, snapshot.val().minaway];
 	console.log(trainInfo);
-
-	//create a for loop to easily create a new table data with  the HTMl from the inputed value 
-	for(var i = 0; i < trainInfo.length;i++){
-		// create a new td
+	for (var i = 0; i < trainInfo.length;i++){
 		var tableData = $('<td>');
-
-		// add the information from the array to the HTMl of this td
 		tableData.html(trainInfo[i]);
-
-		// give it an attribute for save measure 
 		tableData.attr('id', i);
-		// append current td to table row 
 		tableRow.append(tableData)
 	}
-	// append the new row to the last child of the table body
 	$('#trainInfo > tBody:last-child').append(tableRow);
-	// return false to prevent constant returning
 	return false;
 
 // Handle the errors
