3d7ff4a71c0f5cd781bf249688fe1735dd012ae4
Tyler Fallon <Fallon@Tyler-Fallons-MacBook-Pro.local>
2016-06-17 13:38:12 -0400

    data loads from firebase to the table

--- script.js
+++ script.js
@@ -24,20 +24,34 @@ var sessionRef = dataRef.child('trainTime')
 	});
 
 
-dataRef.on('child_added', function(snapshot) {
+dataRef.on("child_added", function(snapshot) {
 
-var tableRow = $('<tr>');
-var trainInfo = [snapshot.val().name, snapshot.val().destination, snapshot.val().time, snapshot.val().frequency];
 
+	// Create a new Table row. outside of the for loop 	
+	var tableRow = $('<tr>');	
+
+	// add all train information to an array for easy use 
+	var trainInfo = [snapshot.val().name, snapshot.val().destination, snapshot.val().time, snapshot.val().frequency];
+	console.log(trainInfo);
+
+	//create a for loop to easily create a new table data with  the HTMl from the inputed value 
+	for(var i = 0; i < trainInfo.length;i++){
+		// create a new td
+		var tableData = $('<td>');
+
+		// add the information from the array to the HTMl of this td
+		tableData.html(trainInfo[i]);
+
+		// give it an attribute for save measure 
+		tableData.attr('id', i);
+		// append current td to table row 
+		tableRow.append(tableData)
+	}
+	// append the new row to the last child of the table body
+	$('#trainInfo > tBody:last-child').append(tableRow);
+	// return false to prevent constant returning
+	return false;
+
+// Handle the errors
 })
-for (var i=0; i<dataRef.length; i++) {
-	var tableData = $('<td>');
-	tableData.html(trainInfo[i]);
-	tableData.attr('id', i);
-	tableRow.append(tableData)
-
-$('#trainInfo > tBody:last-child').append(tableRow);
-return false;
-
-}
 });
